bc4a845b66acbcbeff5f532146fb0c7706e00b78
byhhh2 <rlaalsdl111@naver.com>
2021-02-10 00:30:34 +0900

    movie commit ver2 add todoButton

---
+++ TodoButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Text, TouchableHighlight, StyleSheet} from 'react-native';
+
+const TodoButton = ({onPress, complete, name}) => (
+  <TouchableHighlight
+    onPress={onPress}
+    underlayColor="#efefef"
+    style={styles.button}>
+    <Text
+      style={[
+        styles.text,
+        complete ? styles.complete : null, //complete가 true인지 아닌지에 따라 스타일 적용
+        name === 'Delete' ? styles.deleteButton : null, // name이 delete인지 확인하고 스타일 적용
+      ]}>
+      {name}
+    </Text>
+  </TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'flex-end',
+    padding: 7,
+    borderColor: '#ededed',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginRight: 5,
+  },
+  text: {
+    color: '#666666',
+  },
+  complete: {
+    color: 'green',
+    fontWeight: 'bold',
+  },
+  deleteButton: {
+    color: 'rgba(175,47,47,1)',
+  },
+});
+
+export default TodoButton;
